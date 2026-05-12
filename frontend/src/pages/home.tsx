0684e5f66a98deb793950b2d55f488f6c2e409ad
tfiamietsh <ya.touka2017@yandex.ru>
2026-05-13 01:15:51 +0300

    feat: update home page content

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -2,8 +2,8 @@ import type { FC } from 'preact/compat'
 
 const Home: FC = () => {
   return (
-    <div class="mx-1 p-1 rounded-md bg-slate-950/50">
-      Home
+    <div class="mx-1 p-1 rounded-md bg-slate-950/50 text-center content-center h-16 text-lg">
+      Hi, let's start using Kanban board with login
     </div>
   )
 }
