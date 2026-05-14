72b4b37ce4daf59ec2d77186460082bf99862c62
tfiamietsh <ya.touka2017@yandex.ru>
2026-05-14 22:46:47 +0300

    refactor: replace UserManager.create_user with get_or_create_with

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,17 +1,23 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from __future__ import annotations
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
 
+
 class UserManager(BaseUserManager):
-    def create_user(self, email: str = None, password: str = None) -> None:
+    def get_or_create_with(self, email: str = None, password: str = None) -> tuple[User, bool]:
         if not email:
             raise ValueError('Email required')
         if not password:
             raise ValueError('Password required')
 
-        user = self.model(email=self.normalize_email(email))
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+        user, created = self.get_or_create(email=self.normalize_email(email))
+
+        if created:
+            user.set_password(password)
+            user.save(using=self._db)
+        return user, created
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
@@ -20,6 +26,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
     objects = UserManager()
 
     def __str__(self) -> str:
